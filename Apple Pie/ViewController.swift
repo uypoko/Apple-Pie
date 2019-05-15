3255c4d8e468745eb03b4129165120e6249fa38c
Ryan <cungdinhuy@gmail.com>
2019-05-15 19:08:39 +0700

    create the UI, outlets and actions

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var letterButtons: [UIButton]!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
+    
 
 }
 
